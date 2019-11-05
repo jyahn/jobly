93074e49561aac4a6fb3a9b578ab8d96922d008b
jyahn <jooyoungxahn@gmail.com>
2019-11-04 16:50:25 -0800

    Profile correctly only renders once on page refresh

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -8,39 +8,44 @@ class Profile extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      first_name: '',
-      last_name: '',
-      email: '',
-      photo_url: '',
-      password: '',
-      loading: true,
+      // first_name: '',
+      // last_name: '',
+      // email: '',
+      // photo_url: '',
+      // password: '',
+      // loading: true,
+      // error: []
+      first_name: this.props.currentUser.first_name,
+      last_name: this.props.currentUser.last_name,
+      email: this.props.currentUser.email,
+      photo_url: this.props.currentUser.photo_url,
+      password: this.props.currentUser.password,
       error: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  async componentDidMount() {
-    try {
-      let token = localStorage.getItem('_token');
-      let user = await JoblyApi.getUser(localStorage.getItem("username"))
-      console.log("user is ", user)
-      this.setState(st => ({ ...st, ...user, loading: false }));
-    } catch (err) {
-      this.setState(st => ({
-        error: Array.isArray(err)
-          ? [...st.error, ...err]
-          : [...st.error, err.message]
-      }));
-    }
-  }
+  // async componentDidMount() {
+  //   try {
+  //     let token = localStorage.getItem('_token');
+  //     let user = await JoblyApi.getUser(localStorage.getItem("username"))
+  //     console.log("user is ", user)
+  //     this.setState(st => ({ ...st, ...user, loading: false }));
+  //   } catch (err) {
+  //     this.setState(st => ({
+  //       error: Array.isArray(err)
+  //         ? [...st.error, ...err]
+  //         : [...st.error, err.message]
+  //     }));
+  //   }
+  // }
 
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
     });
   }
-
 
 
   async handleSubmit(evt) {
