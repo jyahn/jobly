a04e5943200d27a2273018ebf4a3a3f81c404793
jyahn <jooyoungxahn@gmail.com>
2019-11-06 20:36:22 -0800

    implemented logic to support filtering by text and salary simultaneously

--- frontend/src/Jobs.js
+++ frontend/src/Jobs.js
@@ -11,13 +11,37 @@ class Jobs extends Component {
     super(props);
     this.state = {
       jobs: [],
+      filteredJobs: [],
       salaryFilter: false,
-      search: ""
+      searchFilter: false,
+      salaryThreshold: '',
+      search: "",
+      searchByWord: ""
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
     this.handleSalaryFilter = this.handleSalaryFilter.bind(this);
+    this.handleUndo = this.handleUndo.bind(this);
+  }
+
+
+  convertSalary(salary) {
+    let result = ''
+    let salaryString = salary.toString();
+    let count = 0;
+    for (let i = salaryString.length; i > 0; i--) {
+      if (count === 3) {
+        result = ',' + result;
+        i++;
+        count = 0;
+      }
+      else {
+        result = salaryString[i - 1] + result;
+        count += 1;
+      }
+    }
+    return '$' + result
   }
 
   async componentDidMount() {
@@ -28,22 +52,52 @@ class Jobs extends Component {
     })
   }
 
+  async handleUndo() {
+    if (this.searchFilter === false) {
+      let jobs = await JoblyApi.getJobs()
+      jobs.sort((a, b) => a.salary - b.salary);
+      this.setState({
+        jobs,
+        salaryFilter: false,
+        salaryThreshold: ''
+      })
+    } else {
+      let jobs = await JoblyApi.getJobsBySearch({ search: this.state.search })
+      jobs.sort((a, b) => a.salary - b.salary);
+      this.setState({
+        jobs,
+        salaryFilter: false,
+        salaryThreshold: ''
+      })
+    }
+  }
+
 
   async handleSalaryFilter(evt, salary) {
-    console.log("salary in filter", salary)
     evt.preventDefault();
-    let jobs = await JoblyApi.getJobsByMinSalary({ salary: salary })
-    console.log("jobs is ", jobs)
-    jobs.sort((a, b) => a.salary - b.salary);
-    this.setState({
-      jobs,
-      salaryFilter: true
-    })
+    if (this.state.searchFilter === false) {
+      let jobs = await JoblyApi.getJobsByMinSalary({ salary: salary })
+      jobs.sort((a, b) => a.salary - b.salary);
+      this.setState({
+        jobs,
+        filteredJobs: jobs,
+        salaryFilter: true,
+        salaryThreshold: salary
+      })
+    } else {
+      let jobs = this.state.filteredJobs.filter(job => (job.salary >= salary))
+      jobs.sort((a, b) => a.salary - b.salary);
+      this.setState({
+        jobs,
+        filteredJobs: jobs,
+        salaryFilter: true,
+        salaryThreshold: salary
+      })
+    }
   }
 
   handleChange(evt) {
     evt.preventDefault();
-    console.log("hello")
     this.setState({
       [evt.target.name]: evt.target.value
     });
@@ -51,45 +105,79 @@ class Jobs extends Component {
 
   async handleSubmit(evt) {
     evt.preventDefault()
-    if (this.state.salaryFilter === false) {
-      let jobs = await JoblyApi.getJobsByMinSalary({ salary: this.state.salaryFilter })
-      jobs.sort((a, b) => a.salary - b.salary);
-      this.setState({
-        jobs
-      })
-    }
-    else {
-      //modify the jobs in state and only return the ones that meet the search criteria.
-      //implement the vice versa logic for if search is submitted first and then salary filter is submitted 2nd.
-    }
-  }
-
-  async handleSearchSubmit(evt) {
-    evt.preventDefault()
-    let jobs = await JoblyApi.getJobsBySearch({ search: this.state.search })
+    let jobs = await JoblyApi.getJobsByMinSalary({ salary: this.state.salaryFilter })
     jobs.sort((a, b) => a.salary - b.salary);
     this.setState({
       jobs
     })
   }
 
+  async handleSearchSubmit(evt) {
+    evt.preventDefault()
+    let searchFilter = true;
+    if (this.state.search === "") {
+      searchFilter = false;
+    }
+    if (this.state.salaryFilter === false) {
+      let jobs = await JoblyApi.getJobsBySearch({ search: this.state.search })
+      jobs.sort((a, b) => a.salary - b.salary);
+      this.setState({
+        jobs,
+        filteredJobs: jobs,
+        searchFilter,
+        searchByWord: this.state.search
+      })
+    } else {
+      if (searchFilter === false) {
+        let jobs = await JoblyApi.getJobsByMinSalary({ salary: this.state.salaryThreshold })
+        jobs.sort((a, b) => a.salary - b.salary);
+        this.setState({
+          jobs,
+          filteredJobs: jobs,
+          searchFilter,
+          searchByWord: this.state.search
+        })
+      }
+      else {
+        let jobs = this.state.filteredJobs.filter(job => (job.title.toLowerCase().includes(this.state.search.toLowerCase())))
+        jobs.sort((a, b) => a.salary - b.salary);
+        this.setState({
+          jobs,
+          filteredJobs: jobs,
+          searchFilter,
+          searchByWord: this.state.search
+        })
+      }
+    }
+  }
+
   render() {
-    console.log("this.state in jobs", this.state)
-    console.log("this.props in jobs", this.props)
     return (
       <div className="Jobs">
         <div className="Jobs-search col-6 mt-3">
           <form
             className="Jobs-searchForm form-group"
             onSubmit={this.handleSearchSubmit}>
-            <input
-              className="Jobs-input form-control"
-              type="text"
-              value={this.state.search}
-              name="search"
-              placeholder="Enter search term.."
-              onChange={this.handleChange} />
-            <Button className="Search-button btn btn-md mt-3">Search</Button>
+            <div className="input-group">
+              <input
+                className="Jobs-input form-control"
+                type="text"
+                value={this.state.search}
+                name="search"
+                placeholder="Enter search term.."
+                onChange={this.handleChange} />
+              <span className="input-group-btn">
+                <Button className="Search-button btn btn-md">Search</Button>
+              </span>
+            </div>
+            {this.state.searchFilter ?
+              <div className="x-jobs">
+                <h6>
+                  {this.state.searchByWord} jobs
+                </h6>
+              </div>
+              : null}
+
           </form>
         </div>
         <table className="Jobs-content row ml-5 mr-5">
@@ -100,33 +188,42 @@ class Jobs extends Component {
                 <h5 className="Salary-header mt-3"><b>Salary estimate</b></h5>
                 <div className="row">
                   <div className="col">
-                    <a href="#"
-                      onClick={(e) => { this.handleSalaryFilter(e, 50000) }}> $50,000+</a> <br />
-                    <a href="#"
-                      onClick={(e) => { this.handleSalaryFilter(e, 75000) }} > $75,000+</a> <br />
-                    <a href="#"
-                      onClick={(e) => { this.handleSalaryFilter(e, 100000) }}> $100,000+</a> <br />
-                    <a href="#"
-                      onClick={(e) => { this.handleSalaryFilter(e, 125000) }} > $125,000+</a> <br />
-                    <a href="#"
-                      onClick={(e) => { this.handleSalaryFilter(e, 150000) }} > $150,000+</a> <br />
-                    <a href="#"
-                      onClick={(e) => { this.handleSalaryFilter(e, 175000) }} > $175,000+</a> <br />
+                    {this.state.salaryThreshold ?
+                      <strong>
+                        {this.convertSalary(this.state.salaryThreshold)} <a href="#" onClick={this.handleUndo}>Undo</a>
+                      </strong>
+                      :
+                      <Fragment>
+                        <a href="#"
+                          onClick={(e) => { this.handleSalaryFilter(e, 50000) }}> $50,000+</a> <br />
+                        <a href="#"
+                          onClick={(e) => { this.handleSalaryFilter(e, 75000) }} > $75,000+</a> <br />
+                        <a href="#"
+                          onClick={(e) => { this.handleSalaryFilter(e, 100000) }}> $100,000+</a> <br />
+                        <a href="#"
+                          onClick={(e) => { this.handleSalaryFilter(e, 125000) }} > $125,000+</a> <br />
+                        <a href="#"
+                          onClick={(e) => { this.handleSalaryFilter(e, 150000) }} > $150,000+</a> <br />
+                        <a href="#"
+                          onClick={(e) => { this.handleSalaryFilter(e, 175000) }} > $175,000+</a> <br />
+                      </Fragment>
+                    }
                   </div>
                 </div>
               </td>
 
               {this.state.jobs.length ? (
                 <td className="container">
-                  <div className="row">
+                  <div className="Jobsss row text-center">
                     {this.state.jobs.map(job => (
                       <div key={job.id} className="Jobs-item col-lg-4 col-md-6 mt-3 px-3">
                         <JobCard job={job} handleClick={this.handleClick} />
                       </div>
                     ))
-                    })
-                </div>
-                </td>) : (<h1>No Jobs Found</h1>
+                    }
+                  </div>
+                </td>)
+                : (<h1>No Jobs Found</h1>
                 )}
             </tr>
           </tbody>
